ramotar/habitica-quest-tracker

commit 3cde5b52b22b2d4e4e89d1ec599298d75d86f857

Improve option validation

questTracker.gs
@@ -241,27 +241,11 @@ function getQuestData(members) {
  * Run this function on the questFinished webhook.
  */
 function updateQuestTracker() {
+  if (!validateOptions()) return;
 
   // open spreadsheet & sheet
-  try {
-    var spreadsheet = SpreadsheetApp.openById(SPREADSHEET_URL.match(/[^\/]{44}/)[0]);
-    var sheet = spreadsheet.getSheetByName(SPREADSHEET_TAB_NAME);
-
-    // if sheet doesn't exist, print error & exit
-    if (sheet === null) {
-      console.log("ERROR: SPREADSHEET_TAB_NAME \"" + SPREADSHEET_TAB_NAME + "\" doesn't exit.");
-      return;
-    }
-  }
-  // if spreadsheet doesn't exist, print error & exit
-  catch (e) {
-    if (e.stack.includes("Unexpected error while getting the method or property openById on object SpreadsheetApp")) {
-      console.log("ERROR: SPREADSHEET_URL not found: " + SPREADSHEET_URL);
-      return;
-    } else {
-      throw e;
-    }
-  }
+  let spreadsheet = SpreadsheetApp.openById(QUEST_TRACKER_SPREADSHEET_URL.match(/[^\/]{44}/)[0]);
+  let sheet = spreadsheet.getSheetByName(QUEST_TRACKER_SPREADSHEET_TAB_NAME);
 
   // if no party, party = user
   let members = api_getPartyMembers();

setup.gs
@@ -180,7 +180,7 @@ function validateOptions() {
     valid = testCredentials();
 
     if (api_getParty().leader.id !== INT_USER_ID) {
-      logWarning("Quest Tracker should only be run by one party member (preferably the party leader).");
+      logInfo("Quest Tracker should only be run by one party member (preferably the party leader).");
     }
   }
 
@@ -206,8 +206,8 @@ function validateOptions() {
     valid = false;
   }
   else if (typeof spreadsheet !== "undefined" && spreadsheet.getSheetByName(SPREADSHEET_TAB_NAME) === null) {
-    logError("SPREADSHEET_TAB_NAME \"" + SPREADSHEET_TAB_NAME + "\" doesn't exist.");
-    valid = false;
+    spreadsheet.insertSheet(SPREADSHEET_TAB_NAME);
+    logWarning("SPREADSHEET_TAB_NAME \"" + SPREADSHEET_TAB_NAME + "\" didn't exist and was created.");
   }
 
   if (!valid) {
@@ -221,7 +221,7 @@ function testCredentials() {
   // [Authors] This function tests the user credentials
 
   try {
-    api_getParty(true);
+    api_getPartyMembers();
   }
   catch (error) {
     if (error.message.startsWith("Request failed") && error.cause.getResponseCode() == 401) {

commit 11161f750017f16b2b88c91a956b3e517b957fa1

Fix naming bug in configuration constants
introduced in 3cde5b52b22b2d4e4e89d1ec599298d75d86f857

questTracker.gs
@@ -244,8 +244,8 @@ function updateQuestTracker() {
   if (!validateOptions()) return;
 
   // open spreadsheet & sheet
-  let spreadsheet = SpreadsheetApp.openById(QUEST_TRACKER_SPREADSHEET_URL.match(/[^\/]{44}/)[0]);
-  let sheet = spreadsheet.getSheetByName(QUEST_TRACKER_SPREADSHEET_TAB_NAME);
+  let spreadsheet = SpreadsheetApp.openById(SPREADSHEET_URL.match(/[^\/]{44}/)[0]);
+  let sheet = spreadsheet.getSheetByName(SPREADSHEET_TAB_NAME);
 
   // if no party, party = user
   let members = api_getPartyMembers();